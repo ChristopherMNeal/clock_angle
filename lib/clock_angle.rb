3b48347ccf18638ca04188c00c9d791b66336763
Christopher Neal <christopher.m.neal@gmail.com>
2021-12-06 15:12:15 -0800

    refactor and pass first two tests
    Co-Authored-By: Chase Stokes <chasehstokes@gmail.com>

--- lib/clock_angle.rb
+++ lib/clock_angle.rb
@@ -1,9 +1,14 @@
 require('pry') 
 
-def clock_angle(time)
-  if time == 12
-    0
+def clock_angle(hours, minutes)
+  hour_hand = Numeric.new()
+  minute_hand = Numeric.new()
+  hour_hand = (((hours.to_f() * 60) / 720)*360)
+  minute_hand = ((minutes.to_f() /60)*360)
+  # binding.pry
+  if (hour_hand - minute_hand).abs > 180
+    360 - (hour_hand - minute_hand).abs
   else
-    360/(12/time)
+    (hour_hand - minute_hand).abs
   end
 end
--- spec/clock_angle_spec.rb
+++ spec/clock_angle_spec.rb
@@ -3,9 +3,12 @@ require('clock_angle')
 
 describe('#clock_angle') do
   it("returns 0 degrees when 12 is entered") do
-    expect(clock_angle(12)).to(eq(0))
+    expect(clock_angle(12, 0)).to(eq(0))
   end
   it("returns 180 degrees when 6 is entered") do
-    expect(clock_angle(6)).to(eq(180))
+    expect(clock_angle(6, 0)).to(eq(180))
   end
+  # it("returns ??? when 12:30 is entered") do
+  #   expect(clock_angle(12, 30)).to(eq(0))
+  # end
 end
