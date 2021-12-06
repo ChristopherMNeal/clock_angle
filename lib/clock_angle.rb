3e5615945d352ce37f2a8c11ea2c1af350b4c6f5
Christopher Neal <christopher.m.neal@gmail.com>
2021-12-06 15:18:36 -0800

    Pass fourth test, function returns smaller angle
    Co-Authored-By: Chase Stokes <chasehstokes@gmail.com>

--- lib/clock_angle.rb
+++ lib/clock_angle.rb
@@ -3,7 +3,7 @@ require('pry')
 def clock_angle(hours, minutes)
   hour_hand = Numeric.new()
   minute_hand = Numeric.new()
-  hour_hand = (((hours.to_f() * 60) / 720)*360)
+  hour_hand = ((((hours.to_f() * 60) + minutes.to_f()) / 720)*360)
   minute_hand = ((minutes.to_f() /60)*360)
   # binding.pry
   if (hour_hand - minute_hand).abs > 180
--- spec/clock_angle_spec.rb
+++ spec/clock_angle_spec.rb
@@ -8,7 +8,10 @@ describe('#clock_angle') do
   it("returns 180 degrees when 6 is entered") do
     expect(clock_angle(6, 0)).to(eq(180))
   end
-  # it("returns ??? when 12:30 is entered") do
-  #   expect(clock_angle(12, 30)).to(eq(0))
-  # end
+  it("returns 165 when 12:30 is entered") do
+    expect(clock_angle(12, 30)).to(eq(165))
+  end
+  it("returns 90 when 9:00 is entered") do
+    expect(clock_angle(9, 00)).to(eq(90))
+  end
 end
